bc8005a5fb7a291f407e3c332f75ef8947db4337
mangalittt <komangalitt1010@gmail.com>
2026-06-10 08:51:37 +0700

    test CI/CD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. Tambahkan baris ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Tambahkan blok kode ini
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
